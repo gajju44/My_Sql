335ffcaa230bc557599f0798f36eebff0d933404
gajju44 <gajjunaphade@gmail.com>
2024-07-21 14:54:25 +0530

    Data types in SQL

---
+++ Data_types.sql
@@ -0,0 +1,51 @@
+--String Data Types
+
+CHAR : String (0 - 255)
+
+VARCHAR : String (0 - 255)
+
+TINYTEXT : String (0 - 255)
+
+TEXT : String (0 - 65535)
+
+BLOB : String (0 - 65535)
+
+MEDIUMTEXT : String (0 - 16777215)
+
+MEDIUMBLOB : String (0 - 16777215)
+
+LONGTEXT : String (0 - 429496­7295)
+
+LONGBLOB : String (0 - 429496­7295)
+
+
+-- Numric Data Types
+
+TINYINT : Integer (-128 to 127)
+
+SMALLINT : Integer (-32768 to 32767)
+
+MEDIUMINT : Integer (-8388608 to 8388607)
+
+INT : Integer (-2147­483648 to 214748­3647)
+
+BIGINT : Integer (-9223­372­036­854­775808 to 922337­203­685­477­5807)
+
+FLOAT : Decimal (precise to 23 digits)
+
+DOUBLE : Decimal (24 to 53 digits)
+
+DECIMAL	: DOU­BLE­ stored as string
+
+
+-- Date & Time Data Types
+
+DATE : yyyy-MM-dd
+
+TIME : hh:mm:ss
+
+DATETIME : yyyy-MM-dd hh:mm:ss
+
+TIMESTAMP : yyyy-MM-dd hh:mm:ss
+
+YEAR : yyyy
